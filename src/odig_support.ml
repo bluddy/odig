1f89b37ec82162577162e69b83b5c9e67ab00a54
Yotam Barnoy <yotambarnoy@gmail.com>
2019-07-08 22:19:57 -0400

    Remove superfluous line

--- src/odig_support.ml
+++ src/odig_support.ml
@@ -66,8 +66,6 @@ module Pkg = struct
       List.sort compare_by_caseless_name (ocaml_pkg () :: pkgs)
     with Failure e -> Log.err (fun m -> m "package list: %s" e); []
 
-  let by_names ?(init = String.Map.empty) pkgs =
-    let add_pkg acc pkg = String.Map.add (name pkg) pkg acc in
   let by_names ?(init = String.Map.empty) ?(use_dirname=false) pkgs =
     let get_name = if use_dirname then out_dirname else name in
     let add_pkg acc pkg = String.Map.add (get_name pkg) pkg acc in
